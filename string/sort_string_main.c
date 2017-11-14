e8002b435ab255cf6b2a4e792d396a6aaeb0b63f
weizhaopeng <WZP_1995@163.com>
2017-11-14 08:02:22 -0800

    字符串数组需要加强太模糊了

--- string/sort_string_main.c
+++ string/sort_string_main.c
@@ -5,11 +5,11 @@ int main (void)
 	for(int i=0;i<5;i++)
 	{
 		printf("请输入字符串%d\n",i+1);
-		scanf("%s",*(strings+i));
+		scanf("%s",strings[i]);
 	}
 	
-	sort_string(strings,5);
-	
+//	sort_string(strings,5);
+
 	printf("排序后的字符串是\n");
 	for(int j=0;j<5;j++)
 	printf("%s\n",strings[j]);
