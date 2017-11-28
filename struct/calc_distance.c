1eeb6be9752abaa11fb46c1b2277805e85edaff9
weizhaopeng <WZP_1995@163.com>
2017-11-28 05:51:20 -0800

    计算长度程序成功

--- struct/calc_distance.c
+++ struct/calc_distance.c
@@ -1,26 +1,33 @@
 #include"stdio.h"
 #include<math.h>
-
-struct posi
+#include<stdlib.h>
+struct point
 {
 	double x;
 	double y;
 };
-typedef struct posi PSI;
+typedef struct point POINT; 
 
-void main (void)
+double distance(POINT *a,POINT *b)
 {
 	double dx,dy,result;
-	PSI *a,*b;
 	
+	dx=a->x-b->x;
+	dy=a->y-b->y;
+ 	result=sqrt(dx*dx+dy*dy);
+	return result;
+}
+void main (void)
+{
+	POINT *a,*b;
+	
+	a=(POINT *)malloc(sizeof(POINT));
+	b=(POINT *)malloc(sizeof(POINT));
+
 	puts("请输入a点的坐标（x-y）：如：3.22 4.33（x y坐标可以是小数）");
 	scanf("%lf%lf",&a->x,&a->y);
 	puts("请输入b点的坐标（x-y）：如：3.22 4.33（x y坐标可以是小数）");
 	scanf("%lf%lf",&b->x,&b->y);
-
-	dx=a->x-b->x;
-	dy=a->y-b->y;
 	
- 	result=sqrt(dx*dx+dy*dy);
-	printf("a b的距离是d=%-5.2f\n",result);
+	printf("a b的距离是d=%-5.2f\n",distance(a,b));
 }
