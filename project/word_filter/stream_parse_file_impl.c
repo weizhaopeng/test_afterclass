490be114a9d0f3aa01ae17be67a170154ccfa13e
weizhaopeng <WZP_1995@163.com>
2018-02-11 22:28:12 +0800

    add word_filter.c, not finish

--- project/word_filter/stream_parse_file_impl.c
+++ project/word_filter/stream_parse_file_impl.c
@@ -1,12 +1,40 @@
 #include others
 #include <stream_parse.h>
 
-int stream_input_parse(char *path)
+char *stream_input_parse(char *path)
 {
-	FILE *fp;
+	FILE	*fp;
+	uint32_t posi;
+	char 	*word, char_temp;
 	
-	p = fopen(path, r+);
+	fp = fopen(path, r+);
+	do {
+		char_temp = (char)fgetc(fp);
+		if(char_temp>='A' && char_temp<='z' || char_temp>='a' && char_temp<='z')
+			word[posi++] = char_temp;
+		else
+		{
+			posi++;
+			break;
+		}
+	}while(1);
+	word[posi] = '\0';
 
+    if(fp_skip_space(fp)==FALSE)
+        return NULL;
+    else
+        return word;
 }
        
+static bool fp_skip_space(FILE *fp)
+{
+	char char_temp;
+    do{
+		char_temp=fgetc(fp);
+        if(char_temp==EOF)
+            return FALSE;
+    }while(char_temp<'A'||(char_temp>'Z'&&char_temp<'a')||char_temp>'z');
+    fseek(fp,-1L,1);
+    return TRUE;
+}
  
--- project/word_filter/word_filter.c
+++ project/word_filter/word_filter.c
@@ -2,19 +2,48 @@
 #include <data_store.h>
 #include <stream_parse.h>
 
-
+#define  CAPACITY 
 
 int main(int argc, char **argv)
 {
 	//step1: 解析参数，参数应该包括需要读取的文件名或者一个网络地址
-	for(int i=1 ; i<argc ; i++)
+	for(int i=1 ; i<argc && argv[i] ; i++)
 	{
-		stream_input_parse(*(argv+i));
+		char *path = argv[i];
+		size_t len;
 
-		
-	step2: 对象初始化，初始化stream buffer和data store
-	step3: stream input流程处理，包括将处理好的word存进stream buffer
-	step4: 依次将stream buffer中的word存进data store中
+		word = stream_input_parse(*(argv+i));
+		len  = strlen(word);
+	//step2: 对象初始化，初始化stream buffer和data store
+		stream_buffer *sb;
+		data_store	  *ds;
+
+		sb = stream_buffer_create(CAPACITY);
+		ds = data_store_create();
+	//step3: stream input流程处理，包括将处理好的word存进stream buffer
+		char  *word;
+		size_t len;
+
+		do {
+			word = stream_input_parse(path);
+			len  = strlen(word);
+			if(word)
+				stream_buffer_insert_word(sb, word, len);
+		} while(1);		
+	//step4: 依次将stream buffer中的word存进data store中
+		do {
+			int errno;
+			errno = stream_buffer_get_word(sb, word);
+			if(errno)
+			{
+				strerror(errno);
+				exit(-1);
+			}
+			else
+			{
+				d
+
+
 	step5: 对data store进行排序，然后获取个数最多的10个word，打印
 备注：有空可以考虑一下，为什么对数据进行多份存储，以及为什么对框架进行这种划分？
 }
