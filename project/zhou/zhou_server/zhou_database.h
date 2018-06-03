42919bb7396843cb26ec8c4b9ce0487ec58153e3
weizhaopeng <WZP_1995@163.com>
2018-06-03 22:54:03 +0800

    the finally patch

--- project/zhou/zhou_server/zhou_database.h
+++ project/zhou/zhou_server/zhou_database.h
@@ -1,6 +1,18 @@
 #ifndef _ZHOU_DATABASE_H_
 #define _ZHOU_DATABASE_H_
 
+typedef struct search_node {
+	char   *hostname;
+	uint8_t isonline;
+	char   *messfile;
+	int 	messlen;
+} search_node;
+
+typedef struct {
+	search_node *root;
+	int    		 node_num;
+};
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
