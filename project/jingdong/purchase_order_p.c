9e73b6cd5d075859f4ded42a955eaf41d4f1b89c
weizhaopeng <WZP_1995@163.com>
2018-09-20 21:19:58 +0800

    小米的测试问题，未完成,结果都是None

--- project/jingdong/purchase_order_p.c
+++ project/jingdong/purchase_order_p.c
@@ -4,39 +4,66 @@
  *3.	对要采购的商品的数量进行排序
  *4.	利用冒泡排序筛选出topn
  *5.	对应的topn相乘相加得到最大最小值
+ *测试数据;
+ 6 5
+ 3 5 1 6 8 1
+ peach
+ grapefruit
+ banana
+ orange
+ orange
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static inline int cmp(const void *a, const void *b);
+typedef struct goods_buf{
+	char name[200];
+	int  num;
+} goods_buf
+
+typedef struct goods {
+	goods_buf *gb;
+	int		   goods_count;
+} goods;
+
+static inline int  cmp(const goods_buf *g1, const goods_buf g2);
 static inline void bsort(int *price, int *topk, int *bottomk);
-static inline void count(const char *temp);
-//存储要买的商品名称和临时的商品名称缓冲区
-char name_buy[1024][200] = {0}, temp[200] = {0};
-//商品名称去重复后的计数， 给出的标签数组
-int  count_buy = 0, name_num[1024] = {0}, goods, num_buy;
+static inline void count(goods *gs, const char *goods_temp);
+static inline int  cmp_price(const int *a, const int *b);
 
 int main(void) {
-	
-	while (scanf("%d%d", &goods, &num_buy) != EOF) {
-		int price[goods];
+	//存储要买的商品名称和临时的商品名称缓冲区
+	int  	   label_num, goods_num;
+	char	   goods_temp[200]
+	goods_buf *gb = calloc(1, sizeof(goods_buf)*1024);
+	goods	  *gs = calloc(1, sizeof(goods));
 
-		for (int i = 0; i < goods; i++)
+	gs->gb = gb;
+	goods_count = 0;
+	
+	while (scanf("%d%d", &label_num, &goods_num) != EOF) {
+		int price[label_num];
+
+		for (int i = 0; i < label_num; i++)
 			scanf("%d", price+i);
 
-		for (int i = 0; i < num_buy; i++) {
-			scanf("%s", temp);
+		for (int i = 0; i < goods_num; i++) {
+			scanf("%s", goods_temp);
 			fflush(stdin);
-			count(temp);
+			count(gs, goods_temp);
 		}
 		//快速排序要买的商品的去重复数量
-		qsort(name_num, count_buy, sizeof(int), cmp);
+		qsort(gs->gb, gs->goods_count, sizeof(goods_buf), cmp);
 
 		//记录价格topk和bottomk数组
 		int topk[count_buy], bottomk[count_buy];
-		//冒泡排序得到
-		bsort(price, topk, bottomk);
+
+		qsort(price, label_num, sizeof(int), cmp_price);
+		for (int i = 0; i < count_buy; i++) {
+			topk[i] = price[i];
+			bottom[i] = price[count_buy-i];
+		}
 
 		long max = 0, min = 0;
 		for (int i = 0; i < count_buy; i++) {
@@ -48,33 +75,21 @@ int main(void) {
 	return 0;
 }
 
-static inline void count(const char *temp) {
+static inline void count(goods *gs, const char *goods_temp) {
 	int i;
 
-	for (i = 0; i < count_buy; i++) {
-		if (strcmp(name_buy[i], temp) == 0)
-			name_num[i]++;
+	for (i = 0; i < gs->goods_count; i++) {
+		if (strcmp(gs->gb[i]->name, goods_temp) == 0)
+			gs->gb[i]->num++;
 	}
-	memcpy(name_buy[i], temp, strlen(temp));
-	name_num[i]++;
+	memcpy(gs->gb[i]->name, goods_temp, strlen(goods_temp));
+	gs->goods_count++;
 }
 
-static inline void bsort(int *price, int *topk, int *bottomk) {
-	for (int i = 0; i < count_buy; i++) {
-		topk[i]   = price[i];
-		bottomk[i] = price[i];
-	}
-
-	for (int i = 0; i < count_buy; i++) {
-		for (int j = i; j < goods; j++) {
-			if (price[i] < price[j])
-				bottomk[i] = price[i];
-			if (price[i] > price[j])
-				topk[i] = price[i];
-		}
-	}
+static inline int cmp(const goods_buf *g1, const goods_buf g2) {
+	return g1->num-g2->num;
 }
-	
-static inline int cmp(const void *a, const void *b) {
-	return *(int*)a-*(int*)b;
+
+static inline int cmp_price(const int *a, const int *b) {
+	return *((int*)a-(int*)b);
 }
